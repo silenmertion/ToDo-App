e1fed91a19aedd4e6c30dac5f8cb7fe3f5273455
Silen Mertion <silenmertion@gmail.com>
2023-06-01 13:50:33 +0300

    save changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
+"use strict";
+// Theme
+
 const todoapp = document.querySelector(".todo-app");
-const themebtn = document.querySelector(".switch__input");
+const themebtn = document.querySelector(".theme-switch__input");
 
 if (localStorage.getItem("theme") === "dark") {
   todoapp.classList.add("dark-theme");
@@ -15,4 +18,30 @@ function themeChange() {
   } else {
     localStorage.setItem("theme", "light");
   }
+}
+
+// Task Working
+
+const listContainer = document.querySelector(".list-container");
+const listItem = document.querySelector(".list-container__item");
+const taskInput = document.querySelector(".task-adder__input");
+const taskBtn = document.querySelector(".task-adder__btn");
+
+taskBtn.addEventListener("click", addTask);
+taskBtn.addEventListener("click", rmTask);
+
+function addTask() {
+  const taskValue = document.querySelector(".task-adder__input").value;
+
+  const taskHTML = ` <li class="list-container__item">${taskValue} <span class="rm-task">\u00d7</span></li>`;
+
+  if (taskValue === "") {
+    alert("This Empty");
+  } else {
+    listContainer.insertAdjacentHTML("beforeend", taskHTML);
+  }
+}
+
+function rmTask() {
+  console.log("Удаление задачи");
 }
